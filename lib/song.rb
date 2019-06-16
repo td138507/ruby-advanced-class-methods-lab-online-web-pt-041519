3077ca1a2406bf3c3c16330ed46a03054c0f5e1a
Taylor Doyle <tcd56@cornell.edu>
2019-06-16 22:18:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,59 @@ class Song
     self.class.all << self
   end
 
-  def create
-    @@all
+  def self.create
+    song = self.new
+    song.save
+    song
   end
-  
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name 
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    components = filename.split(" - ")
+    artist_name = components[0]
+    name = comonents[0]
+    name = components[1].gsub(".mp3", "")
+    song = self.new
+    song.artist_name = artist_name
+    song.name = name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    components = filename.split(" - ")
+    artist_name = components[0]
+    name = components[1].gsub(".mp3", "")
+    song = self.create
+    song.artist_name = artist_name
+    song.name = name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+    
 end
